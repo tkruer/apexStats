353655cc0e57fb3cd8fa8af467da43459c345ada
Tyler K <tylerkruer1@gmail.com>
2022-09-28 10:46:09 -0700

    changes to file struct

---
+++ apexStats/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  apexStats
+//
+//  Created by Tyler Kruer on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
