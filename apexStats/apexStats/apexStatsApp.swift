adf0e25cc14503fc7e87ddd0fab67d3e4bdc3736
Tyler K <tylerkruer1@gmail.com>
2022-09-28 10:37:45 -0700

    init commit

---
+++ apexStats/apexStats/apexStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  apexStatsApp.swift
+//  apexStats
+//
+//  Created by Tyler Kruer on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct apexStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
